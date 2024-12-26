f47c6d71a1c5cdaab11a711fc76e993b49fc950a
leiyi2 <leiyi2@tuhu.cn>
2024-12-26 15:47:56 +0800

    init project

---
+++ order-writer-service/src/main/java/com/leiyi/start/ServiceApplication.java
@@ -0,0 +1,17 @@
+package com.leiyi.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages={"com.leiyi"})
+@ComponentScan(basePackages={"com.leiyi"})
+public class ServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+}
